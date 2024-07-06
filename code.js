6d5ac894cbe1e6355bd1723950667b1783b2f22b
ElMansari-Youssef <91879610+ElMansari-Youssef@users.noreply.github.com>
2024-07-06 04:47:55 +0200

    added more styles and js

--- code.js
+++ code.js
@@ -1,5 +1,29 @@
 let CAContainer = document.querySelector(".CartAvatarContainer svg");
 let myItems = document.querySelector(".myCartItems");
+let mainImg = document.getElementById("Main-image");
+let close = document.querySelector(".closeSvg img");
+
+
+close.addEventListener("click", () => {
+
+    let lightbox = document.querySelector(".lightBox");
+    let header = document.querySelector(".header");
+    lightbox.classList.remove("visible");
+    header.classList.remove("visible");
+})
+
+
+mainImg.addEventListener("click", () => {
+   
+    let lightbox = document.querySelector(".lightBox");
+    let header = document.querySelector(".header");
+
+    lightbox.classList.add("visible");
+    header.classList.add("visible");
+
+})
+
+
 
 CAContainer.addEventListener("click", () => {
 
@@ -7,11 +31,11 @@ CAContainer.addEventListener("click", () => {
 })
 
 
-document.addEventListener("click", () =>{
+document.addEventListener("click", () => {
 
-    if (!event.target.closest(".CartAvatarContainer")){
+    if (!event.target.closest(".CartAvatarContainer")) {
 
         myItems.classList.remove("show");
     }
-    
+
 })
