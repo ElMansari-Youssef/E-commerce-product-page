20ac4adc758a294b2806ae1034a268fdacfb3afa
ElMansari-Youssef <91879610+ElMansari-Youssef@users.noreply.github.com>
2024-07-10 03:35:44 +0200

    added some js

--- code.js
+++ code.js
@@ -1,5 +1,6 @@
 let CAContainer = document.querySelector(".CartAvatarContainer svg");
 let myItems = document.querySelector(".myCartItems");
+
 let mainImg = document.getElementById("Main-image");
 let close = document.querySelector(".closeSvg img");
 let thumbnails = document.querySelectorAll(".imageContainer .thumbNails img");
@@ -14,18 +15,40 @@ let emptyCart = document.querySelector(".myCartItems .emptyCart");
 let item3 = document.querySelector(".ItemsNumber .item3");
 let item1 = document.querySelector(".ItemsNumber .item1");
 
+let CAContainer2 = document.querySelector(".CartAvatarContainer .myCartItems .checkoutItems");
+let checkoutItems = document.querySelector(".checkoutItems .checkOut");
+
+let deleteSvg = document.querySelector(".checkOut svg:last-child")
 
 
+deleteSvg.addEventListener("click", () => {
+
+    let checkoutItemsContainer = document.querySelector(".checkoutItems");
+    let root = document.querySelector(":root");
+
+    checkoutItemsContainer.classList.remove("checkoutItemsVisibility");
+
+    emptyCart.classList.remove("hideEmptyCardtxt");
+
+    root.style.setProperty("--switcher2", "hidden");
+    root.style.setProperty("--opacityy", "0");
+    root.style.setProperty("--NbrOfCheckoutItems", "''");
+
+    document.querySelector(".ItemsNumber .item2 span").innerHTML = 0;
+
+    
+
+})
 
 item1.addEventListener("click", () => {
 
     let spanText = document.querySelector(".ItemsNumber .item2 span").innerHTML;
     let span2Number = parseInt(spanText);
     let finalSpan = span2Number - 1;
-    if(finalSpan >= 0){
-    document.querySelector(".ItemsNumber .item2 span").innerHTML = finalSpan
+    if (finalSpan >= 0) {
+        document.querySelector(".ItemsNumber .item2 span").innerHTML = finalSpan
     }
-    
+
 })
 
 item3.addEventListener("click", () => {
@@ -42,17 +65,21 @@ addTocard.addEventListener("click", () => {
 
     let multiplier = document.querySelector(".ItemsNumber .item2 span").innerHTML;
     let checkoutItems = document.querySelector(".checkoutItems");
+
     let numbersOfitems = document.querySelector(".checkoutItems .checkOut .price .multiplier");
     let itemPrice = document.querySelector(".checkoutItems .checkOut .price .itemPrice");
     let total = document.querySelector(".checkoutItems .checkOut .price .total");
 
     emptyCart.classList.add("hideEmptyCardtxt");
     checkoutItems.classList.toggle("checkoutItemsVisibility");
-    
+
     numbersOfitems.innerHTML = multiplier;
-   
+
 
     total.innerHTML = "$" + (parseInt(itemPrice.textContent) * parseInt(numbersOfitems.innerHTML)).toFixed(2);
+
+    checkContainer(CAContainer2, checkoutItems);
+
 
 })
 
@@ -125,3 +152,23 @@ document.addEventListener("click", () => {
     }
 
 })
+
+
+
+
+
+function checkContainer(parent, child) {
+
+
+    if (parent.contains(child)) {
+        let root = document.querySelector(":root");
+        root.style.setProperty("--switcher2", "visible");
+        root.style.setProperty("--opacityy", "1");
+        root.style.setProperty("--NbrOfCheckoutItems", "'1'");
+        // console.log(root.style); 
+    }
+    else {
+        console.log("not exists");
+    }
+
+}
